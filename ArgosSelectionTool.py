e93296d9ff31ce204d6fd861a2cb7bd5f6833c89
ivkuehn <isabelle.kuehn@duke.edu>
2026-09-21 12:53:32 -0400

    Initial commit of Argos Selection Tool script

---
+++ ArgosSelectionTool.py
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------
+# ArgosSelectionTool.py
+#
+# Description: Reads in an Argos tracking data file and allows
+#   the user to identify the tracked sitings found within a 
+#   specified bounding box.
+#
+# Author: Isabelle Kuehn(isabelle.kuehn@duke.edu)
+# Date:   Fall 2026
+#--------------------------------------------------------------
+
