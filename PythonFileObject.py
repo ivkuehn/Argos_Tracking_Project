ceec6e080ea87da44fa46c9cbdfbe85a1baf7128
ivkuehn <isabelle.kuehn@duke.edu>
2026-09-23 12:29:19 -0400

    Created file to check python file opening methods.

---
+++ PythonFileObject.py
@@ -0,0 +1,17 @@
+# Test code for file opening methods in Python
+
+#Set a variable to the CSV filename
+the_filename = 'data/raw/MoveBank/Satellite tracking of black-capped petrels 2019-argos.csv'
+#Create a file object pointing to file name
+f = open(the_filename,'r')
+
+#Create a list of all the lines in the file via the file object
+line_list = f.readlines()
+#Close the file
+f.close()
+#Print the 11th item in the line list
+print(line_list[10])
+
+with open('data/raw/MoveBank/Satellite tracking of black-capped petrels 2019-argos.csv','r') as f:
+	line_list = f.readlines()
+print(line_list[10])
